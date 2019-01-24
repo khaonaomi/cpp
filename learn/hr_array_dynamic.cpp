0d93154c3b00b22fefbbb4c3d9e2aed058f9d3cd
khaonaomi <khaonaomi@outlook.com>
2019-01-24 08:43:21 +0700

    a

---
+++ learn/hr_array_dynamic.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int len;
+    cin >> len;
+    int *a = new int[len];
+    for (int i = 0; i < len; i++) {
+        cin >> a[i];
+    }
+    for (int j = len - 1; j >= 0; j++) {
+        cout << a[j];
+        if (j == 0) { cout << endl; } else { cout << " "; }
+    }
+    delete[] a;
+    return 0;
+}
